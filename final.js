2d6d96509cd2bf0d66c4d984af090457f9c84d08
Leo Fujita <setsunal.fujita@gmail.com>
2016-01-18 13:10:46 +0900

    implemented dragging, need fix

--- final.js
+++ final.js
@@ -1,7 +1,9 @@
 var images = {};
+var defaultX = {};
+var defaultY = {};
+var X = {};
+var Y = {};
 var context = document.getElementById('canvas').getContext("2d");
-var charX = 450;
-var charY = 400;
 var totalResources = 5;
 var loadedCounter = 0;
 var fps = 30;
@@ -15,7 +17,9 @@ var srcs = [
 ];
 
 var isDragging = false;
+var dragTarget = null;
 
+//initial loading
 for (i in srcs){
   images[i] = new Image();
   images[i].onload = function() { 
@@ -25,10 +29,11 @@ for (i in srcs){
     }
   }
   images[i].src = "images/" + srcs[i] + ".png";
-  console.log(i + srcs[i] + images[i].src);
+
+  //console.log(i + srcs[i] + images[i].src);
 }
 
-function drawSoup(centerX, centerY, width, height) {
+function drawSoup(centerX, centerY, width, height, color) {
   context.beginPath();
   context.moveTo(centerX, centerY - height/2);
   context.bezierCurveTo(
@@ -39,94 +44,92 @@ function drawSoup(centerX, centerY, width, height) {
       centerX - width/2, centerY + height/2,
       centerX - width/2, centerY - height/2,
       centerX, centerY - height/2);
-  context.fillStyle = "linen";
+  context.fillStyle = color;
   context.fill();
   context.closePath();  
 }
 
-function redraw() {
-  var x = charX;
-  var y = charY;
-
-  canvas.width = canvas.width;  // clears the canvas 
-
+function drawDefault(){
+  //set canvas size to window size
   context.canvas.width = window.innerWidth;
   context.canvas.height = window.innerHeight;
+  // clears canvas
+  //context.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawSoup(canvas.width/5*2, canvas.height/2, 1200, 600);
+  drawSoup(canvas.width/5*2, canvas.height/2, 1200, 600, String("linen"));
   for (i in images){
-    context.drawImage(images[i], canvas.width/5*4, canvas.height/6*i);
+    defaultX[i] = canvas.width/5*4;
+    defaultY[i] = canvas.height/6*i;
+
+    context.drawImage(images[i], defaultX[i], defaultY[i]);
   }
 }
 
-/*
-// ドラッグ開始
-var mouseDown = function(e) {
-  // ドラッグ開始位置
-  var posX = parseInt(e.clientX - canvas.offsetLeft);
-  var posY = parseInt(e.clientY - canvas.offsetTop);
+function redraw() {
+  drawDefault();
+  for (i in images){
+    X[i] = defaultX[i];
+    Y[i] = defaultY[i];
+    context.drawImage(images[i], X[i], Y[i]);
+  }
+}
 
-  for (var i = images.length - 1; i >= 0; i--) {
-    // 当たり判定（ドラッグした位置が画像の範囲内に収まっているか）
-    if (posX >= images[i].drawOffsetX &&
-        posX <= (images[i].drawOffsetX + images[i].drawWidth) &&
-        posY >= images[i].drawOffsetY &&
-        posY <= (images[i].drawOffsetY + images[i].drawHeight)
+//HOW ABOUT SETTING EACH XS AS E.CLIENTXS
+
+// begin drag
+var mouseDown = function(e) {
+  console.log("mouse: " + e.clientX + ", " + e.clientY);
+  for (i in images) {
+    console.log(images[i] + X[i] + Y[i]);
+    console.log("natural: " + images[i].naturalWidth + images[i].naturalHeight)
+    // hit check whether cursor is on the image
+    if (e.clientX >= X[i] &&
+        e.clientX <= (X[i] + images[i].naturalWidth) &&
+        e.clientY >= Y[i] &&
+        e.clientY <= (Y[i] + images[i].naturalHeight)
        ) {
       dragTarget = i;
       isDragging = true;
+      console.log("down: " + dragTarget);
       break;
     }
   }
 }
 
-// ドラッグ終了
+// end drag
 var mouseUp = function(e) {
   isDragging = false;
+  dragTarget = null;
 };
 
-// canvasの枠から外れた
+// end drag on mouse out of canvas
 var mouseOut = function(e) {
   mouseUp(e);
 }
 
-// ドラッグ中
+// during drag
 var mouseMove = function(e) {
-  // ドラッグ終了位置
-  var posX = parseInt(e.clientX - canvas.offsetLeft);
-  var posY = parseInt(e.clientY - canvas.offsetTop);
-
   if (isDragging) {
-    // canvas内を一旦クリア
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    // clears canvas
+    //context.clearRect(0, 0, canvas.width, canvas.height);
 
-    var x = 0;
-    var y = 0;
     for (var i in images) {
       if (i == dragTarget) {
-        x = posX - images[i].drawWidth / 2;
-        y = posY - images[i].drawHeight / 2;
-
-        // ドラッグが終了した時の情報を記憶
-        images[i].drawOffsetX = x;
-        images[i].drawOffsetY = y;
-      } else {
-        x = images[i].drawOffsetX;
-        y = images[i].drawOffsetY;
+        X[i] = e.clientX - images[i].naturalWidth / 2;
+        Y[i] = e.clientY - images[i].naturalHeight / 2;
+      }
+      else {
+        X[i] = defaultX[i];
+        Y[i] = defaultY[i];
       }
 
-      // 画像を描画
-      context.drawImage(images["tofu"], canvas.width/5*4, canvas.height/6);
-      context.drawImage(images["carrot"], canvas.width/5*4, canvas.height/6*2);
-      context.drawImage(images["mushroom"], canvas.width/5*4, canvas.height/6*3);
-      context.drawImage(images["meat"], canvas.width/5*4, canvas.height/6*4);
-      context.drawImage(images["onion"], canvas.width/5*4, canvas.height/6*5);
+      console.log(i + ": " + X[i] + ", " + Y[i]);
+      context.drawImage(images[i], X[i], Y[i]); //uncomment for shabby dragging
     }
   }
 }
-*/
 
-// canvasにイベント登録
+// make canvas listen for events (can add multiple events unlike HTML)
 canvas.addEventListener('mousedown', function(e){mouseDown(e);}, false);
 canvas.addEventListener('mousemove', function(e){mouseMove(e);}, false);
 canvas.addEventListener('mouseup',   function(e){mouseUp(e);},   false);
